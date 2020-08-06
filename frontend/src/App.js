9f3bec14e6a9b2e380d93b88b52e2b34acb5a49c
ChrisJMacdonald <chrisjmacdonald123@gmail.com>
2020-08-06 13:21:08 +1200

    Learning Material UI, Cleaned up Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,18 +11,6 @@ function App() {
       <MainAppBar/>
       <header className="App-header">
       <LoginTab/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       
     </div>
