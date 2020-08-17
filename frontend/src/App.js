221b190f8314ce79a39aeae49099201583865e3c
megan4132 <megan4132@gmail.com>
2020-08-17 13:48:39 +1200

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
   // const [loggedIn, setLoggedIn] = React.useState(0)
   const [cookie, setCookie] = useCookies(["user_level"]);
   const [page,setPage] = React.useState(0)
-  console.log("Logged status: ",cookie)
+  console.log("Logged status: ", cookie)
   const resetPage = (newPage)=>(page === newPage? setPage(0) : setPage(newPage))
 
   
@@ -31,10 +31,10 @@ function App() {
       < MainAppBar  setPage = {resetPage} setLogged = {(status)=>{ setCookie("user_level", status, { path: '/' }) }} loggedIn = {cookie.user_level}/>
       {/* ONLY THE LOGIN PAGE IS DISPLAYED IF THE USER LEVEL IS 0 */}
       {/* eslint-disable-next-line*/}
-      {cookie.user_level!=0 ? null: <LoginTab setLogged = {(status)=>{ setCookie("user_level", status, { path: '/' }) }} loggedIn = {cookie.user_level>0} />}
-      {page === pageIndex["deliveryreporting"] && cookie.user_level>2 ? <DeliveryReporting/> : null}
-      {page === pageIndex["reporting"] && cookie.user_level>2 ? <Reporting/> : null}
-      {page === pageIndex["delivererportal"] && cookie.user_level>2 ? <DelivererPortal/> : null}
+      {cookie.user_level != 0 ? null : <LoginTab setLogged = {(status)=>{ setCookie("user_level", status, { path: '/' }) }} loggedIn = {cookie.user_level>0} />}
+      {page === pageIndex["deliveryreporting"] && cookie.user_level > 2 ? <DeliveryReporting/> : null}
+      {page === pageIndex["reporting"] && cookie.user_level > 2 ? <Reporting/> : null}
+      {page === pageIndex["delivererportal"] && cookie.user_level > 2 ? <DelivererPortal/> : null}
     </div>
   );
 }
