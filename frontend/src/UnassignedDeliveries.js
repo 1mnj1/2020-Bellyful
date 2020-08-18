6efb744c2e5c35e9decaaa75c39421edecf7ef83
megan4132 <megan4132@gmail.com>
2020-08-19 07:25:13 +1200

    added support for multilines in the list

--- frontend/src/UnassignedDeliveries.js
+++ frontend/src/UnassignedDeliveries.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Text} from "react"
 import $ from 'jquery'
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -8,13 +8,12 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: 800,
+    maxWidth: 400,
     // textAlign: "center",
     backgroundColor: theme.palette.background.paper,
   },
@@ -42,22 +41,28 @@ export default function UnassignedDeliveries (props) {
     // To get the data
     React.useEffect(() => {
         
-        $.post( props.url ,props.form,  function( returnable ) {
+        $.post( props.url, function(returnable) {
         if(returnable === null) return 
         if (returnable === undefined) return 
         if(returnable.length === 0) return 
+
         var fields = Object.keys(returnable[0])
+        var values = Object.values(returnable[0])
+        console.log('fields from object.keys', fields)
+        console.log('values from object.values', values)
+
         const cols = $(setColumns(fields))
-        console.log("before logging the columns")
-        console.log("VolunteersCols = ",cols)
-        console.log("before logging the objects")
-        console.log("VolunteersObj = ",returnable)
+        console.log("before logging the columns for props", props.title)
+        console.log("columns = ",cols)
+        console.log("before logging the objects for props", props.title)
+        console.log("deliveries = ",returnable)
         
         // To use an encapsulated function, put a dollar in front of it (it just works ?!)
-        $(setState(state => ({ ...state,columns:cols.toArray(), data : returnable})))
+        // $(setState(state => ({ ...state,columns:cols.toArray(), data : returnable})))
+        $(setState(state => ({ ...state,columns:cols.toArray(), data : values})))
         // this.props.setLogged(true)
     });
-    }, [props.url,props.form ]);
+    }, [props.url,props.title]);
 
 
 
@@ -81,41 +86,135 @@ export default function UnassignedDeliveries (props) {
   
       setChecked(newChecked);
     };
+
+
+    // const contents = this.state.data.forEach(item => {
+    //   // change the title and location key based on your API
+    //   return (
+    //     <ListItemText 
+    //     // id={labelId} 
+    //     primary={item.Recipient}
+    //     secondar={item.Meals} 
+    //     />
+    //   )
+
+    // });
   
     return (
+      <div>
+        <h2>New Deliveries</h2> 
       <List className={classes.root}>
-        {[0, 1, 2, 3].map((value) => {
+        {/* {[0, 1, 2, 3].map((value) => { */}
+        {state.columns.map((value) => {
           const labelId = `checkbox-list-label-${value}`;
   
           return (
-            <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-              {/* the following line breaks the page */}
-              <ListItemText id={labelId} primary={state.data.Volunteer} />
+            <div>
+              <div>
+                
+                <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checked.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemIcon>
+                  {/* {contents} */}
+                  {/* <ListItemText id={labelId} primary={state.data} secondary="secondline text"/> */}
+                  {/* the following line breaks the page */}
+                  <ListItemText 
+                    id={labelId} 
+                    primary={'Julia Jones'}
+                    // primary={state.columns.Recipient}
+                    secondary={labelId}
+                  />
 
-              {/* title= {null}
-        columns={state.columns}
-        data={state.data}
-        icons={tableIcons} */}
+                  {/* title= {null}
+            columns={state.columns}
+            data={state.data}
+            icons={tableIcons} */}
 
-              {/* <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="comments">
-                  <CommentIcon />
-                </IconButton>
-              </ListItemSecondaryAction> */}
-            </ListItem>
+                  {/* <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="comments">
+                      <CommentIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction> */}
+                </ListItem>
+
+                <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checked.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemIcon>
+        
+                  <ListItemText 
+                    id={labelId} 
+                    primary={'Megan Johnson'}
+                    secondary={'32 Clemows Lane, Albany'}
+                  />
+
+                </ListItem>
+                <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
+                  <ListItemIcon>
+                    <Checkbox
+                      edge="start"
+                      checked={checked.indexOf(value) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemIcon>
+                 
+                  <ListItemText
+                    primary="Michael Hill"
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className={classes.inline}
+                          color="textPrimary"
+                        >
+                          123 Street, Albany
+                        </Typography>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className={classes.inline}
+                          color="textPrimary"
+                          style={{whiteSpace: 'pre-line'}}
+                        >
+                          {'\n'} 021123456789
+                        </Typography>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className={classes.inline}
+                          style={{whiteSpace: 'pre-line'}} 
+                        >
+                        {/* the style property allows the use of the newline character */}
+                        {'\n'} 2 Meals
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                </ListItem>
+              </div>
+              
+            </div>
           );
         })}
       </List>
+      <button>I can do this!</button>
+      </div>
     );
 
 
@@ -252,5 +351,6 @@ export default function UnassignedDeliveries (props) {
 //         );
 //       })}
 //     </List>
-//   );
+//   );›
 // }
+
