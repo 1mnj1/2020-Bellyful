314bd016c1e145eaf76c18169e05e50f6de3551d
megan4132 <megan4132@gmail.com>
2020-08-25 10:44:37 +1200

    quantity state working

--- frontend/src/FreezerLog.js
+++ frontend/src/FreezerLog.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import $ from 'jquery'
 
 import Alert from '@material-ui/lab/Alert';
@@ -10,7 +10,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 
-
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,16 +24,43 @@ const useStyles = makeStyles((theme) => ({
 
 export default function FreezerLog (props) {
 
-    const [state, setState] = React.useState({
+    const [state, setState] = useState({
         columns: [ {}, ],
         data: [ {}, ],
+        quantities: [{}]
     });
+
+    const [quantity, setQuantity] = useState([]);
+
+    const updateFieldChanged = index => e => {
+        console.log('index', index);
+        console.log('property name:', e.target.name);
+        console.log('property value:', e.target.value);
+        let newArray = [...quantity]; // Copy the old state
+        newArray[index] = e.target.value; // replace e.target.value with updated quantity
+
+        setQuantity(newArray); // set the new state
+
+        console.log('updated array', newArray);
+    }
 
     const setColumns = (colNames)=>{
       var columns = []; 
       colNames.forEach(element => columns.push({title: element, field: element}));
       return columns
   }
+
+    // Return a series of text elements to make a form
+    var saveForm = ()=> {
+      var formData = $("form.mealForm").serializeArray()
+      if(formData.length === 0){
+        formData = [{}]
+        props.setForm(formData)
+        return
+      } 
+      
+    
+    };
 
 
     function setAlert() {
@@ -43,6 +71,40 @@ export default function FreezerLog (props) {
         {console.log("alert function called")}
       </Alert>
       )
+    }
+
+    function getAddressID( dict, success ){
+      $.post("http://"+window.location.hostname+":3000/delivery/getAddress",dict,success)
+    }
+
+    var saveForm = ()=> {
+      var formData = $("form.referrerForm").serializeArray()
+      if(formData.length === 0){
+        formData = [{}]
+        props.setForm(formData)
+      } 
+      // getAddressID(formData, (add_id)=>{
+      //   formData.push({"name":"address_id", "value" : add_id})
+      //   getPersonID( formData, (person_id)=> {
+      //     formData.push({"name":"person_id", "value" : person_id})
+      //     props.setForm(formData)
+      //   })
+        
+      // })
+      
+    
+    };
+
+    var saveMealQuantities = () => {
+
+    }
+
+
+    function mealChange(mealTypeId, quantity) {
+        return (
+          console.log("Hello there, ", {mealTypeId}, "quantity has been altered to ", {quantity})
+          // <Alert>Hello there, {mealTypeId} quantity has been altered</Alert>
+        )
     }
 
     
@@ -75,39 +137,52 @@ export default function FreezerLog (props) {
 
   const classes = useStyles();
 
+
+  const handleTopUp = (evt) => {
+    evt.preventDefault();
+    alert('Submitting quantity ${quantity} and mealTypeId ${mealTypeId')
+    // for (mealType : row) {
+    //   row[state.columns[0]];
+    // }
+  }
+
   const createList = state.data.map((row) => {
-    const value = row[state.columns[0]]
-    console.log("Row: ",row, "Value: ", value)
+    const mealTypeId = row[state.columns[0]]
+    // console.log("Row: ",row, "Value: ", mealTypeId)
   
-    console.log("row 1 (Meal Type Id): ", row[state.columns[0]])
-    console.log("row 2 (Dish): ", row[state.columns[1]])
-    console.log("row 3 (Available Meals): ", row[state.columns[2]])
+    // console.log("row 1 (Meal Type Id): ", row[state.columns[0]])
+    // console.log("row 2 (Dish): ", row[state.columns[1]])
+    // console.log("row 3 (Available Meals): ", row[state.columns[2]])
+
+    // let quantity = 0;
+    // const [quantity, setQuantity] = useState(0);
 
     return (
       <div>
         <div>
-          <ListItem key={value} role={undefined} dense button>          
+          <ListItem key={mealTypeId} role={undefined} dense button>  
+          {console.log("list item mealTypeId", mealTypeId)}        
             <ListItemText
               primary = {
                 <React.Fragment>
                   <Typography
-                    component="span"
-                    variant="body2"
+                    component="h5"
+                    variant="h5"
                     className={classes.inline}
                     color="textPrimary"
                   >
-                    {row[state.columns[1]]}
+                    {row[state.columns[1]]}<br/>
                   </Typography>
                   <Typography
-                    component="span"
-                    variant="body2"
+                    component="h6"
+                    variant="h6"
                     className={classes.inline}
                     color="textPrimary"
                     style={{whiteSpace: 'pre-line'}}
                   >
-                    : 
+                    Current Meal Count: <strong>{row[state.columns[2]]}</strong>
                   </Typography>
-                  <Typography
+                  {/* <Typography
                     component="span"
                     variant="body2"
                     className={classes.inline}
@@ -115,7 +190,31 @@ export default function FreezerLog (props) {
                     style={{whiteSpace: 'pre-line'}}
                   >
                     &nbsp;{row[state.columns[2]]}
-                  </Typography>
+                  </Typography> */}
+                  {/* Might not need a form, can just have the textfield but would need to style it better */}
+                  <form className = "mealForm" style = {props.class}>
+                    {/* onChange = {saveForm}  */}
+                    <TextField
+                      className = {classes.textField}
+                      id = "mealChangeQuantity"
+                      label = "Add/remove meals"
+                      defaultValue = "0"
+                      placeholder = "0"
+                      type = "number"
+                      fullWidth
+                      name = "mealChange2"
+                      value = {quantity[mealTypeId-1]} // The value of the state at the mealTypeId index
+                      // onChange={mealChange(mealTypeId, quantity)}
+                      // onChange={e => setQuantity(e.target.value)}
+                      onChange = {updateFieldChanged(mealTypeId - 1)}
+                      // onChange={event => {
+                      //   let quantities = state.quantities;
+                      //   quantities[mealTypeId - 1] = event.target.value
+                      //   setState(quantities)}
+                      // }
+                    />
+                    <br/>
+                  </form>
                 </React.Fragment>
               }
              
@@ -126,17 +225,20 @@ export default function FreezerLog (props) {
       </div>
     );
   })
+  
   return (
     <div style = {{overflowX: "hidden"}}>
       <h2>{props.title}</h2> 
     <List className={classes.root}>
       {createList}
     </List>
-
+    <div>
+      <br/>
     <Button 
       variant="contained" 
       color="secondary" 
-      onClick={setAlert}
+      // onClick={setAlert}
+      onClick={handleTopUp}
     >
       Top up
     </Button>
@@ -148,6 +250,15 @@ export default function FreezerLog (props) {
     >
       Taken
     </Button>
+    </div>
+    {/* quick way to show the buttons without them being cut off by the bottom navigation bar */}
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+
 
     </div>
   );
