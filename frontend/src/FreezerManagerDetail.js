7b81f235f29b394e26a07862b29429bc197e93f3
megan4132 <megan4132@gmail.com>
2020-09-02 15:48:15 +1200

    removed unused code

--- frontend/src/FreezerManagerDetail.js
+++ frontend/src/FreezerManagerDetail.js
@@ -2,7 +2,7 @@ import React, {useState} from "react"
 import $ from 'jquery'
 
 import Button from '@material-ui/core/Button';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -19,7 +19,6 @@ import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    // textAlign: "center",
     backgroundColor: theme.palette.background.paper,
   }
 
