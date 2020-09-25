185bcc5ff8b0cf186a33487d2c80b49acd2a5217
megan4132 <megan4132@gmail.com>
2020-09-25 19:24:16 +1200

    added dynamic background colour

--- frontend/src/c_Portals/c_Delivery/StyledButton.js
+++ frontend/src/c_Portals/c_Delivery/StyledButton.js
@@ -9,25 +9,31 @@ const mobileCheck = function() {
   return window.screen.width < 620
 };
 
-const StyledButton = withStyles({
-  root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    borderRadius: 3,
-    border: 0,
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    width: mobileCheck()?"30%":"36%", 
-    float : 'left', 
-    marginLeft : '10%' 
-  },
-  label: {
-    textTransform: 'capitalize',
-  },
-})(Button);
 
 
-export default function ClassesShorthand() {
-  return <StyledButton>classes shorthand</StyledButton>;
+
+export default function ClassesShorthand(props) {
+  const StyledButton = withStyles({
+    root: {
+      // background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      width: mobileCheck()?"30%":"36%", 
+      float : 'left', 
+      marginLeft : '10%',
+      borderRadius: 10, 
+      fontWeight : 'bold', 
+      backgroundColor: props.customColor
+  
+    },
+    label: {
+      textTransform: 'none',
+    },
+  })(Button);
+
+
+  return <StyledButton>Classes Shorthand</StyledButton>;
 }
